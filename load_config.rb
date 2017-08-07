721fc3056ba5086821d1f3305f24d4149ea61960
arthur <arthur.karganyan@gmail.com>
2017-08-07 17:46:41 +0300

    CONFIG var

--- load_config.rb
+++ load_config.rb
@@ -2,12 +2,12 @@ require 'yaml'
 require "redis"
 
 path = File.expand_path(File.dirname(__FILE__)) + '/'
-config = YAML.load_file(path + 'config.yml')
-CAPTAIN_REDIS_DB = config['captain_redis_db']
+CONFIG = YAML.load_file(path + 'config.yml')
+CAPTAIN_REDIS_DB = CONFIG['captain_redis_db']
 
-$redis = Redis.new(db: config['telegram_redis_db'] || fail)
+$redis = Redis.new(db: CONFIG['telegram_redis_db'] || fail)
 $captain_redis = Redis.new(db: CAPTAIN_REDIS_DB || fail)
 
-BOT_LOGGER = Logger.new(path + config['telegram_log_path'])
-CAPTAIN_LOGGER = Logger.new(path + config['captain_log_path'])
-SECRETS_HASH = config["secrets"]
+BOT_LOGGER = Logger.new(path + CONFIG['telegram_log_path'])
+CAPTAIN_LOGGER = Logger.new(path + CONFIG['captain_log_path'])
+SECRETS_HASH = CONFIG["secrets"]
